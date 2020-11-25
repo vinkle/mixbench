87bc8379841c05f74b3bfccbc7dbe7782d701659
vinkle <722672+vinkle@users.noreply.github.com>
2020-11-25 16:43:09 +0100

    Update main-cuda.cpp

--- mixbench-cuda/main-cuda.cpp
+++ mixbench-cuda/main-cuda.cpp
@@ -7,6 +7,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cuda.h>
+#include <unistd.h> 
+#include <pthread.h> 
 #include <cuda_runtime.h>
 #include <string.h>
 #include "lcutil.h"
@@ -16,8 +18,61 @@
 #ifdef READONLY
 #define VECTOR_SIZE (32*1024*1024)
 #else
-#define VECTOR_SIZE (8*1024*1024)
+#define VECTOR_SIZE (256*1024*1024)
 #endif
+#define NUM_ITERATIONS 1000
+
+void *gputhread0(void *vargp) 
+{
+	cudaSetDevice(0);
+	for(int i = 0; i < NUM_ITERATIONS; ++i)
+	{
+		double *c;
+		c = (double*)malloc(datasize);
+		mixbenchGPU(c, VECTOR_SIZE);
+		free(c);
+	}
+} 
+
+void *gputhread1(void *vargp) 
+{
+	cudaSetDevice(1);
+	for(int i = 0; i < NUM_ITERATIONS; ++i)
+	{
+		double *c;
+		c = (double*)malloc(datasize);
+		mixbenchGPU(c, VECTOR_SIZE);
+		free(c);
+	}
+} 
+
+
+void *gputhread2(void *vargp) 
+{
+	cudaSetDevice(2);
+	for(int i = 0; i < NUM_ITERATIONS; ++i)
+	{
+		double *c;
+		c = (double*)malloc(datasize);
+		mixbenchGPU(c, VECTOR_SIZE);
+		free(c);
+	}
+} 
+
+
+void *gputhread3(void *vargp) 
+{
+	cudaSetDevice(3);
+	for(int i = 0; i < NUM_ITERATIONS; ++i)
+	{
+		double *c;
+		c = (double*)malloc(datasize);
+		mixbenchGPU(c, VECTOR_SIZE);
+		free(c);
+	}
+} 
+
+
 
 int main(int argc, char* argv[]) {
 #ifdef READONLY
@@ -36,12 +91,18 @@ int main(int argc, char* argv[]) {
 	printf("Total GPU memory %lu, free %lu\n", totalCUDAMem, freeCUDAMem);
 	printf("Buffer size:          %dMB\n", datasize/(1024*1024));
 	
-	double *c;
-	c = (double*)malloc(datasize);
-
-	mixbenchGPU(c, VECTOR_SIZE);
-
-	free(c);
+	pthread_t thread_id0, thread_id1, thread_id2, thread_id3;
+	pthread_create(&thread_id0, NULL, gputhread0, NULL);
+	pthread_create(&thread_id1, NULL, gputhread1, NULL);
+	pthread_create(&thread_id2, NULL, gputhread2, NULL);
+	pthread_create(&thread_id3, NULL, gputhread3, NULL);
+	pthread_join(thread_id0, NULL); 
+	pthread_join(thread_id1, NULL); 
+	pthread_join(thread_id2, NULL); 
+	pthread_join(thread_id3, NULL); 
+	
+	printf("Finishing processing Thread\n"); 
+	
 
 	return 0;
 }
